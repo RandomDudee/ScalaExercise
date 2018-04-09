58fd07e11b8763dd6e7e1ffd7361c4c5690dfaca
Sean Rickards <seanrickards56@gmail.com>
2018-04-10 00:59:49 +0100

    Jacks Futures

--- LottoFutures.scala
+++ LottoFutures.scala
@@ -41,4 +41,36 @@ object LottoFutures extends App {
   Thread.sleep(5000)
 }
 
+// Jack's Work
+//
+////This is just a random function that will return an Int of 500000 sets of 5 numbers after 3 seconds
+////I've changed it so it returns whether it has been found
+//def ticketsMultiple: Boolean = {
+//  val multipleTickets = for (i <- 1 to 500001) yield util.Random.shuffle(1 to 90).splitAt(5)._1.sortWith(_ < _)
+//  val singleTicket = util.Random.shuffle(1 to 90).splitAt(5)._1
+//  val winLose: Boolean = multipleTickets.contains(singleTicket)
+//
+//  winLose
+//}
+//
+////When we call this, it will be Future<Incomplete> until randomFunction returns (3 seconds)
+//def mySimpleFuture: Future[Boolean] = Future {
+//  ticketsMultiple
+//}
+////Assign a val so we can add a callback
+//val futureInstance = mySimpleFuture
+//
+////Changes above allow us to pattern match against the future
+////Using if statements here are called guards
+//futureInstance.onComplete {
+//  case Success(result) if result => println("Found")
+//  case Success(result) if !result => println("Not found")
+//  case Failure(error) => println(error)
+//}
+//
+////Keep the JVM running on the main thread, or it will shut down before our future
+////returns
+////Keeeping it alive a little longer
+//Thread.sleep(40000)
+//}
 
