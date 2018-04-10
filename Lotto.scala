940b02d4208253b5204316ae6fc7e4704f7a2393
Sean Rickards <seanrickards56@gmail.com>
2018-04-10 17:10:15 +0100

    Trying new things

--- Lotto.scala
+++ Lotto.scala
@@ -5,55 +5,57 @@ import scala.util.Random._
 import Threads._
 import com.sun.xml.internal.bind.v2.TODO
 
+import scala.collection.immutable
+
 object Lotto extends App {
 
-  //var multipleTickets = 0
+//  //var multipleTickets = 0
+//
+//  val thread1 = new Thread(new Runnable() {
+//    def run(): Unit =
+//      while (true) {
+//        val multipleTickets = for (i <- 1 to 500000) yield util.Random.shuffle(1 to 90).splitAt(5)._1.sortWith(_ < _)
+//        val firstSet = multipleTickets
+//      }
+//  })
+////  TODO Try and inject firstSet into thread3
+//
+//  val thread2 = new Thread(new Runnable() {
+//    def run(): Unit =
+//      while (true) {
+//        val multipleTickets = for (i <- 500001 to 1000000) yield util.Random.shuffle(1 to 90).splitAt(5)._1.sortWith(_ < _)
+//        val secondSet = multipleTickets
+//      }
+//  })
+////  TODO Try and inject secondSet into thread3
+//
+//
+//  val thread3 = new Thread(new Runnable() {
+//    def run(): Unit =
+//      while (true) {
+//        val random = util.Random.shuffle(1 to 90)
+//
+//        val fiveNumbers = random.splitAt(5)
+//
+//        val singleTicket = fiveNumbers._1
+//
+//        val multipleTickets = for (i <- 1 to 5) yield util.Random.shuffle(1 to 90).splitAt(5)._1.sortWith(_ < _)
+//
+//        val draw = singleTicket.sortWith(_ < _)
+//
+//        val winLose = multipleTickets.contains(draw)
+//
+//        if (winLose == true)println("Found") else println("None Found")
+//
+//      }
+//  })
+///*  TODO looks like thread 3 is only taking its own 5 tickets and checking against them and not taking the 1 million tickets from threads 1 & 2
+//    TODO also looks like thread3 keeps generating a new draw only need it to create one draw and match against threads 2 & 3
+// */
+//
 
-  val thread1 = new Thread(new Runnable() {
-    def run(): Unit =
-      while (true) {
-        val multipleTickets = for (i <- 1 to 500000) yield util.Random.shuffle(1 to 90).splitAt(5)._1.sortWith(_ < _)
-        val firstSet = multipleTickets
-      }
-  })
-//  TODO Try and inject firstSet into thread3
 
-  val thread2 = new Thread(new Runnable() {
-    def run(): Unit =
-      while (true) {
-        val multipleTickets = for (i <- 500001 to 1000000) yield util.Random.shuffle(1 to 90).splitAt(5)._1.sortWith(_ < _)
-        val secondSet = multipleTickets
-      }
-  })
-//  TODO Try and inject secondSet into thread3
-
-
-  val thread3 = new Thread(new Runnable() {
-    def run(): Unit =
-      while (true) {
-        val random = util.Random.shuffle(1 to 90)
-
-        val fiveNumbers = random.splitAt(5)
-
-        val singleTicket = fiveNumbers._1
-
-        val multipleTickets = for (i <- 1 to 5) yield util.Random.shuffle(1 to 90).splitAt(5)._1.sortWith(_ < _)
-
-        val draw = singleTicket.sortWith(_ < _)
-
-        val winLose = multipleTickets.contains(draw)
-
-        if (winLose == true)println("Found") else println("None Found")
-
-      }
-  })
-/*  TODO looks like thread 3 is only taking its own 5 tickets and checking against them and not taking the 1 million tickets from threads 1 & 2
-    TODO also looks like thread3 keeps generating a new draw only need it to create one draw and match against threads 2 & 3
- */
-
-
-
-
+//
 //    val random = util.Random.shuffle(1 to 90)
 //
 //    val fiveNumbers = random.splitAt(5) // should have just took 5 instead of split
@@ -62,32 +64,78 @@ object Lotto extends App {
 //
 //
 //      //   val list = List(singleTicket)
+
+
+      //do { list } while (list < 5 )
+
+    def multipleTickets: immutable.Seq[Seq[Int]] = for (i <- 1 to 1000000) yield util.Random.shuffle(1 to 90).take(5).sortWith(_ < _)
+      // Got help of jack as I could not get loops working. Didn't know Yield was a thing.... HAd single ticket and drew it 5 times  instead of 5 random tickets changed to work.
+
+//    def multipleTickets2 = for (i <- 250001 to 500000) yield util.Random.shuffle(1 to 90).take(5).sortWith(_ < _)
 //
+//    def multipleTickets3 = for (i <- 500001 to 750000) yield util.Random.shuffle(1 to 90).take(5).sortWith(_ < _)
 //
-//      //do { list } while (list < 5 )
+//    def multipleTickets4 = for (i <- 750001 to 1000000) yield util.Random.shuffle(1 to 90).take(5).sortWith(_ < _)
+
+    def draw = util.Random.shuffle(1 to 90).take(5).sortWith(_ < _) // (_ < _) sorts by ascending order
+
+    def winLose = multipleTickets.contains(draw) // kept failing saying false. swapped round args.
+
+    def four = util.Random.shuffle(1 to 90).take(4).sortWith(_<_)
+
+    def matchFour = multipleTickets.contains(four)
+
+    val three: Seq[Int] = util.Random.shuffle(1 to 90).take(3).sortWith(_<_)
+
+    val matchThree =  three.map{x => multipleTickets.map(_.contains(x))}
+
+    val one = util.Random.shuffle(1 to 90).take(1).sortWith(_<_).head
+
+    val matchOne: immutable.Seq[Boolean] = multipleTickets.map(_.contains(one))
+
+//    val winLose2 = multipleTickets2.contains(draw)
 //
-//    val multipleTickets = for (i <- 1 to 500) yield util.Random.shuffle(1 to 90).splitAt(5)._1.sortWith(_ < _)
-//      // Got help of jack as I could not get loops working. Didn't know Yield was a thing.... HAd single ticket and drew it 5 times  instead of 5 random tickets changed to work.
+//    val winLose3 = multipleTickets3.contains(draw)
 //
-//    val draw = singleTicket.sortWith(_ < _) // (_ < _) sorts by ascending order
+//    val winLose4 = multipleTickets4.contains(draw)
+
+    if (winLose == true)println("Found") else println("None Found")
+
+    if (matchFour == true)println("Found Four") else println("None Found Four")
+
+    if (matchThree.count(_ == true) >= 1)println("Found Three") else println("None Found Three")
+
+    if (matchOne.count(_ == true) >= 1)println("Found One") else println("None Found One")
+
+//    if (winLose2 == true)println("Found 2") else println("None Found 2")
 //
-//    val winLose = multipleTickets.contains(draw) // kept failing saying false. swapped round args.
-////
-////     println(draw)
-////     println(multipleTickets)
-//   //println(thread1)
-//   // println(thread2)
+//    if (winLose3 == true)println("Found 3") else println("None Found 3")
+//
+//    if (winLose4 == true)println("Found 4") else println("None Found 4")
+
+
+
+//
+//     println(draw)
+//     println(multipleTickets)
+   //println(thread1)
+   // println(thread2)
 //
 //    println(winLose)
-//
+//    println(winLose2)
+//    println(winLose3)
+//    println(winLose4)
+
+
 //  //test line
 
 
 //
 //
-thread1.
-thread1.start()
-thread2.start()
-thread3.start()
+//thread1.
+//thread1.start()
+//thread2.start()
+//thread3.start()
 }
+
 
--- LottoFutures.scala
+++ LottoFutures.scala
@@ -4,73 +4,73 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object LottoFutures extends App {
 
-  //This is just a random function that will return an Int of 500000 sets of 5 numbers after 3 seconds
-  def ticketsMultiple: Unit = {
-    Thread.sleep(3000)
-    val multipleTickets = for (i <- 1 to 500001) yield util.Random.shuffle(1 to 90).splitAt(5)._1.sortWith(_ < _)
-    val singleTicket = util.Random.shuffle(1 to 90).splitAt(5)._1
-    val winLose = multipleTickets.contains(singleTicket)
-
-    if (winLose == true)println("Found") else println("None Found")
-
-  }
-
-
-
-
-
-  //When we call this, it will be Future<Incomplete> until randomFunction returns (3 seconds)
-  def mySimpleFuture: Future[Unit] = Future {
-    ticketsMultiple
-  }
-
-
-  //Assign a val so we can add a callback
-  val futureInstance = mySimpleFuture
-
-  //This is the callback, after the 3 seconds, this will be called.
-  //It should always succeed, but the compiler doesn't like it if we don't put in a
-  //failure case
-  futureInstance.onComplete {
-    case Success(result) => println(ticketsMultiple)
-    case Failure(error) => println(error)
-  }
-
-  //Keep the JVM running on the main thread, or it will shut down before our future
-  //returns
-  Thread.sleep(5000)
-}
+//  //This is just a random function that will return an Int of 500000 sets of 5 numbers after 3 seconds
+//  def ticketsMultiple: Unit = {
+//    Thread.sleep(3000)
+//    val multipleTickets = for (i <- 1 to 500001) yield util.Random.shuffle(1 to 90).splitAt(5)._1.sortWith(_ < _)
+//    val singleTicket = util.Random.shuffle(1 to 90).splitAt(5)._1
+//    val winLose = multipleTickets.contains(singleTicket)
+//
+//    if (winLose == true)println("Found") else println("None Found")
+//
+//  }
+//
+//
+//
+//
+//
+//  //When we call this, it will be Future<Incomplete> until randomFunction returns (3 seconds)
+//  def mySimpleFuture: Future[Unit] = Future {
+//    ticketsMultiple
+//  }
+//
+//
+//  //Assign a val so we can add a callback
+//  val futureInstance = mySimpleFuture
+//
+//  //This is the callback, after the 3 seconds, this will be called.
+//  //It should always succeed, but the compiler doesn't like it if we don't put in a
+//  //failure case
+//  futureInstance.onComplete {
+//    case Success(result) => println(ticketsMultiple)
+//    case Failure(error) => println(error)
+//  }
+//
+//  //Keep the JVM running on the main thread, or it will shut down before our future
+//  //returns
+//  Thread.sleep(5000)
+//}
 
 // Jack's Work
-//
-////This is just a random function that will return an Int of 500000 sets of 5 numbers after 3 seconds
-////I've changed it so it returns whether it has been found
-//def ticketsMultiple: Boolean = {
-//  val multipleTickets = for (i <- 1 to 500001) yield util.Random.shuffle(1 to 90).splitAt(5)._1.sortWith(_ < _)
-//  val singleTicket = util.Random.shuffle(1 to 90).splitAt(5)._1
-//  val winLose: Boolean = multipleTickets.contains(singleTicket)
-//
-//  winLose
-//}
-//
-////When we call this, it will be Future<Incomplete> until randomFunction returns (3 seconds)
-//def mySimpleFuture: Future[Boolean] = Future {
-//  ticketsMultiple
-//}
-////Assign a val so we can add a callback
-//val futureInstance = mySimpleFuture
-//
-////Changes above allow us to pattern match against the future
-////Using if statements here are called guards
-//futureInstance.onComplete {
-//  case Success(result) if result => println("Found")
-//  case Success(result) if !result => println("Not found")
-//  case Failure(error) => println(error)
-//}
-//
-////Keep the JVM running on the main thread, or it will shut down before our future
-////returns
-////Keeeping it alive a little longer
-//Thread.sleep(40000)
-//}
+
+//This is just a random function that will return an Int of 5000000 sets of 5 numbers after 3 seconds
+//I've changed it so it returns whether it has been found
+def ticketsMultiple: Boolean = {
+  val multipleTickets = for (i <- 1 to 500000) yield util.Random.shuffle(1 to 90).take(5).sortWith(_ < _)
+  val singleTicket = util.Random.shuffle(1 to 90).take(5).sortWith(_ < _)
+  val winLose: Boolean = multipleTickets.contains(singleTicket)
+
+  winLose
+}
+
+//When we call this, it will be Future<Incomplete> until randomFunction returns (3 seconds)
+def mySimpleFuture: Future[Boolean] = Future {
+  ticketsMultiple
+}
+//Assign a val so we can add a callback
+val futureInstance = mySimpleFuture
+
+//Changes above allow us to pattern match against the future
+//Using if statements here are called guards
+futureInstance.onComplete {
+  case Success(result) if result => println("Found")
+  case Success(result) if !result => println("Not found")
+  case Failure(error) => println(error)
+}
+
+//Keep the JVM running on the main thread, or it will shut down before our future
+//returns
+//Keeeping it alive a little longer
+Thread.sleep(40000)
+}
 
